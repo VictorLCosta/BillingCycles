ae65fc58c793f03561b78cfe0d36184f28da14e7
VictorLCosta <victorlc2019@outlook.com>
2022-05-24 15:25:05 -0300

    Frontend remaded

---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,9 @@
+const App = (props: any) => {
+    return (
+        <div className="app">
+            App
+        </div>
+    )
+}
+
+export default App
---
+++ frontend/webpack/webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript"
+                        ],
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+                type: 'asset/inline'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        path: path.resolve(__dirname, '..', './build'),
+        filename: 'app.js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new webpack.ProvidePlugin({
+            "React": "react",
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', './public/index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'app.css'
+        }),
+        new ForkTsCheckerWebpackPlugin({
+            async: false,
+            devServer: true
+        })
+    ],
+    devServer: {
+        static: path.join(__dirname, "..", "build"),
+        compress: true,
+        port: 8000,
+    }
+}
