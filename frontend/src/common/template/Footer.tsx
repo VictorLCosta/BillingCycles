2d9c30c1997076c8c3046a9c8d6207cf49e259cd
VictorLCosta <victorlc2019@outlook.com>
2022-05-27 11:49:26 -0300

    Frontend - Footer

---
+++ frontend/src/common/template/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = (props: any) => {
+    return (
+        <footer className="main-footer">
+            <strong>
+                Copyright &copy; 2022
+                <a href="https://github.com/VictorLCosta" target="_blank"> VictorLCosta</a>
+            </strong>
+        </footer>
+    )
+}
+
+export default Footer
--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -2,12 +2,17 @@ import '../common/template/dependencies'
 
 import Header from '../common/template/Header'
 import SideBar from '../common/template/SideBar'
+import Footer from '../common/template/Footer'
 
 const App = (props: any) => {
     return (
         <div className="wrapper">
             <Header/>
             <SideBar/>
+            <div className="content-wrapper px-4 py-2">
+                Conteudo
+            </div>
+            <Footer/>
         </div>
     )
 }
